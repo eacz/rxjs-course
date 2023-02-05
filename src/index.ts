b24e271b78e0c99e8ce07580002a6cd0e9932dcc
Esteban Canteros <estebanacanteros@gmail.com>
2023-02-04 23:13:06 -0300

    first obsevable

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
+import { Observable } from 'rxjs'
+// <string>
+const obs$ = new Observable<string>((subscriber) => {
+  subscriber.next('Hello')
+  subscriber.next('World')
 
+  subscriber.next('Hello')
+  subscriber.next('World')
+  subscriber.complete()
 
-console.log('Hola Mundo!');
+  // These values will not be listened, 'cause they're after the complete method
+  subscriber.next('Hello')
+  subscriber.next('World')
+})
 
-
-
-
-
-
-
+obs$.subscribe((res) => console.log(res))
