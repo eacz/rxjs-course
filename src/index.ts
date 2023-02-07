6a8d368bc32875e34f6771acc701400b271babd4
Esteban Canteros <estebanacanteros@gmail.com>
2023-02-06 23:48:51 -0300

    handling a subscriber success cases with callbacks

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,13 @@ const obs$ = new Observable<string>((subscriber) => {
 
   subscriber.next('Hello')
   subscriber.next('World')
+
+  //Force error
+  //const a = undefined
+  //a.active = true
+
+  subscriber.next('If there is an error this message will not show')
+
   subscriber.complete()
 
   // These values will not be listened, 'cause they're after the complete method
@@ -13,4 +20,11 @@ const obs$ = new Observable<string>((subscriber) => {
   subscriber.next('World')
 })
 
-obs$.subscribe((res) => console.log(res))
+obs$.subscribe(
+  //callback called when .next() call is successful
+  (value) => console.log('next: ', value),
+  //callback called when there is an error
+  (error) => console.warn('error: ', error),
+  //callback called when the Observable call the .complete() method successfully
+  () => console.info('complete')
+)
